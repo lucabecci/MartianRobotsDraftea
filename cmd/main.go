f665c875d73cd760ed742c82cba97226e48d8d8c
lucabecci <beccibrian@gmail.com>
2022-04-06 23:59:40 -0300

    feat(): cmd main added as entrypoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/lucabecci/MartianRobotsDraftea/internal/calculation"
+	"github.com/lucabecci/MartianRobotsDraftea/pkg/file"
 )
 
 func main() {
-	fmt.Println("cmd of the project")
-
+	data, err := file.ProcessFile()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	robots, err := calculation.Process(data)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for x, robot := range robots {
+		fmt.Println("ROBOT", x, robot)
+	}
 }
